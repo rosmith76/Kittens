f38ca6396cb61276151b5c2b22c5a6cd1f17bc2e
rosmith76 <robbe.smith@gmail.com>
2017-07-21 14:32:46 -0400

    GA JS Circuits exercise
    Intro to jQuery
    
    Followed through project

---
+++ main.js
@@ -0,0 +1,16 @@
+// Shorthand version
+$(function() {
+    console.log("Hello world");
+
+    // Select element by id
+    $("#thumbnails");
+
+    // Select element by class
+    $('.thumbnail');
+
+    // Select a HTML element
+    $('ul');
+
+    // Select element with a nested selector
+    $('ul li');
+});
